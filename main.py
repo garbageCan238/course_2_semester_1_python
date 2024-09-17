4b90d938459707853768773d0c98aac12d0f4245
Rashid <Rashid@WINDHELM>
2024-09-17 10:58:17 +0300

    add something and time

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import random
 import time
 import copy
 
+
 class UninterruptiblePowerSupply:
-    possible_manufacturers = ["Apple", "Samsung", "Hp", "Dell", "Xiaomi", "Huawei", "Techno", "Google", "OnePlus", "Alcatel", "Asus", "Lenovo", "Vivo"]
+    possible_manufacturers = ["Apple", "Samsung", "Hp", "Dell", "Xiaomi", "Huawei", "Techno", "Google", "OnePlus",
+                              "Alcatel", "Asus", "Lenovo", "Vivo"]
     possible_brands = ["S1", "S2", "S3", "Redmi", "Nova", "Spark", "A60", "Go", "K15", "Galaxy", "15", "13", "B256"]
 
     def __init__(self):
@@ -11,11 +13,29 @@ class UninterruptiblePowerSupply:
         self.brand = random.choice(self.possible_brands)
         self.capacity = random.randint(0, 100_000)
 
+    def print_info(self):
+        print(
+            f"manufacturer: {self.manufacturer}, brand: {self.brand}, capacity: {self.capacity}")
+
     def __repr__(self):
         return f"UninterruptiblePowerSupply(manufacturer={self.manufacturer}, brand={self.brand}, capacity={self.capacity})"
 
 
 class PowerSupplies:
+    @staticmethod
+    def by_capacity(x, y):
+        return x.capacity > y.capacity
+
+    @staticmethod
+    def byBrand(x, y):
+        return x.brand > y.brand
+
+    @staticmethod
+    def byManufacturer(x, y):
+        return x.manufacturer > y.manufacturer
+
+    currentMethod = None
+
     def __init__(self, count):
         self.supplies = [UninterruptiblePowerSupply() for _ in range(count)]
 
@@ -28,6 +48,10 @@ class PowerSupplies:
 
     def __setitem__(self, index, value):
         self.supplies[index] = value
+
+    def print_array(self):
+        for i in range(len(self.supplies)):
+            self.supplies[i].print_info()
 
     def selection_sort(self, compare):
         for j in range(self.count - 1):
@@ -80,43 +104,62 @@ class PowerSupplies:
         for i in range(self.count - 1):
             for j in range(i + 1, 0, -1):
                 if compare(self.supplies[j - 1], self.supplies[j]):
-                    self.supplies[j], self.supplies[j-1] = self.supplies[j-1], self.supplies[j]
+                    self.supplies[j], self.supplies[j - 1] = self.supplies[j - 1], self.supplies[j]
 
 
 def main():
-    count = 5000
-    print(f"Sorting {count} power supplies by capacity...")
-
+    count = 100
     original = PowerSupplies(count)
+
+    print("Введите поле, по которому вы хотите сортировать массив. 1 - По изготовителю, 2 - По бренду, 3 - По вместимости.")
+    a = input()
+    if a == "1":
+        original.currentMethod = original.byManufacturer
+    elif a == "2":
+        original.currentMethod = original.byBrand
+    elif a == "3":
+        original.currentMethod = original.by_capacity
+    else:
+        print("Введите 1, 2 или 3")
+        return
+
+    original.print_array()
+
+    print(f"Sorting {count} power supplies")
     power_supplies = copy.deepcopy(original)
     start_time = time.time()
-    power_supplies.selection_sort(lambda x, y: x.capacity > y.capacity)
+    power_supplies.selection_sort(original.currentMethod)
     elapsed_time = time.time() - start_time
-    print(f"Selection sort, time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Insertion sort, time elapsed: {elapsed_time:.6f} seconds")
+    power_supplies.print_array()
 
     power_supplies = copy.deepcopy(original)
     start_time = time.time()
-    power_supplies.bubble_sort(lambda x, y: x.capacity > y.capacity)
+    power_supplies.bubble_sort(original.currentMethod)
     elapsed_time = time.time() - start_time
-    print(f"Bubble sort, time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Insertion sort, time elapsed: {elapsed_time:.6f} seconds")
+    power_supplies.print_array()
 
     power_supplies = copy.deepcopy(original)
     start_time = time.time()
-    power_supplies.shaker_sort(lambda x, y: x.capacity > y.capacity)
+    power_supplies.shaker_sort(original.currentMethod)
     elapsed_time = time.time() - start_time
-    print(f"Shaker sort, time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Insertion sort, time elapsed: {elapsed_time:.6f} seconds")
+    power_supplies.print_array()
 
     power_supplies = copy.deepcopy(original)
     start_time = time.time()
-    power_supplies.shell_sort(lambda x, y: x.capacity > y.capacity)
+    power_supplies.shell_sort(original.currentMethod)
     elapsed_time = time.time() - start_time
-    print(f"Shell sort, time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Insertion sort, time elapsed: {elapsed_time:.6f} seconds")
+    power_supplies.print_array()
 
     power_supplies = copy.deepcopy(original)
     start_time = time.time()
-    power_supplies.insertion_sort(lambda x, y: x.capacity > y.capacity)
+    power_supplies.insertion_sort(original.currentMethod)
     elapsed_time = time.time() - start_time
-    print(f"Insertion sort, time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Insertion sort, time elapsed: {elapsed_time:.6f} seconds")
+    power_supplies.print_array()
 
 
 if __name__ == "__main__":
